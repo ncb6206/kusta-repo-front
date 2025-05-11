599999ef197eddaab5a06791e80c676717bdfd4d
ncb6206 <ncb6206@gmail.com>
2025-05-12 02:56:47 +0900

    Feat : 링크, 에러 관련 상수 적용

---
+++ src/constants/api.ts
@@ -0,0 +1,33 @@
+export const NETWORK = {
+  RETRY_COUNT: 2,
+  TIMEOUT: 10000,
+} as const;
+
+export const HTTP_STATUS_CODE = {
+  SUCCESS: 200,
+  CREATED: 201,
+  NO_CONTENT: 204,
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  NOT_FOUND: 404,
+  CONTENT_TOO_LARGE: 413,
+  INTERNAL_SERVER_ERROR: 500,
+} as const;
+
+export const HTTP_ERROR_MESSAGE = {
+  [HTTP_STATUS_CODE.NOT_FOUND]: {
+    HEADING: '페이지가 없는 것 같습니다.',
+    BODY: '요청하신 페이지를 찾을 수 없습니다.',
+    BUTTON: '홈으로 가기',
+  },
+  [HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR]: {
+    HEADING: '현재 페이지를 표시할 수 없습니다.',
+    BODY: `잠시 후 다시 시도해주세요.`,
+    BUTTON: '새로고침',
+  },
+  [HTTP_STATUS_CODE.BAD_REQUEST]: {
+    HEADING: '잘못된 요청입니다.',
+    BODY: '확인 후 다시 시도해주세요.',
+    BUTTON: '홈으로 가기',
+  },
+} as const;
--- src/constants/path.ts
+++ src/constants/path.ts
@@ -6,3 +6,10 @@ export const PATH = {
   SCHOOL_DETAIL: (schoolId: string) => `/school/${schoolId}`,
   RELOAD: 0,
 } as const;
+
+export const LINK_PATH = {
+  KUSTA: 'https://www.kusta.or.kr',
+  YOUTUBE: 'https://www.youtube.com/@GO_KUSTA',
+  INSTAGRAM: 'https://www.instagram.com/go.kusta',
+  FACEBOOK: 'https://www.facebook.com/go.kusta/?locale=ko_KR',
+} as const;
