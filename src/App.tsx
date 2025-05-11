ee48a098cc6b24595ff52063966fcd32aca5ef97
ncb6206 <ncb6206@gmail.com>
2025-05-12 02:54:22 +0900

    Feat : 페이지 레이아웃 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Outlet } from 'react-router-dom';
+
+import { useResetError } from '@/hooks/common/useResetError';
+import ErrorBoundary from '@/components/common/ErrorBoundary/ErrorBoundary';
+import Error from '@/components/common/Error/Error';
+import Header from '@/components/common/Header';
+import Footer from '@/components/common/Footer';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { handleErrorReset } = useResetError();
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <ErrorBoundary Fallback={Error} onReset={handleErrorReset}>
+      <div className="flex min-h-screen w-screen flex-col bg-white">
+        <Header />
+        <main className="flex-1">
+          <Outlet />
+        </main>
+        <Footer />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </ErrorBoundary>
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import AppRouter from '@/router/AppRouter.tsx';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { queryClient } from '@/hooks/api/queryClient';
+import { QueryClientProvider } from '@tanstack/react-query';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const main = async () => {
+  const root = createRoot(document.querySelector('#root') as Element);
+
+  root.render(
+    <StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <AppRouter />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </StrictMode>,
+  );
+};
+
+main();
