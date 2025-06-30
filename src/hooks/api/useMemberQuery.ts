8bb07a2ee0526633aa167b4c80ca0754138bdcd7
ncb6206 <ncb6206@gmail.com>
2025-07-01 00:13:03 +0900

    Refactor : react query 수정

--- src/hooks/api/useMemberQuery.ts
+++ src/hooks/api/useMemberQuery.ts
@@ -1,9 +1,10 @@
 import { useQuery } from '@tanstack/react-query';
 
 import { getMember } from '@/api/getMember';
+import { MemberData } from '@/types/member';
 
 export const useMemberQuery = (id: string) => {
-  const { data: MemberData } = useQuery({
+  const { data: MemberData } = useQuery<MemberData>({
     queryKey: ['member', id],
     queryFn: () => getMember(id),
   });
--- src/hooks/api/useRaceQuery.ts
+++ src/hooks/api/useRaceQuery.ts
@@ -1,9 +1,10 @@
 import { useQuery } from '@tanstack/react-query';
 
 import { getRace } from '@/api/getRace';
+import { RaceData } from '@/types/race';
 
 export const useRaceQuery = (id: string) => {
-  const { data: RaceData } = useQuery({
+  const { data: RaceData } = useQuery<RaceData>({
     queryKey: ['race', id],
     queryFn: () => getRace(id),
   });
