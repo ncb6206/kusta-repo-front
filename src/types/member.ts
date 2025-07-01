9b8004c6047832ce8b5a0d8076f95887bc0979f5
ncb6206 <ncb6206@gmail.com>
2025-07-02 00:55:09 +0900

    Type : 타입 변경

--- src/types/member.ts
+++ src/types/member.ts
@@ -1,3 +1,5 @@
+import { RaceDataType } from '@/types/race';
+
 export interface SearchMemberData {
   success: boolean;
   message: string;
@@ -12,15 +14,36 @@ export interface MemberData {
 
 export interface MemberDataType {
   memberNo: number;
-  universityNo?: number;
+  universityNo: number;
   memberName: string;
-  memberProfile: string;
-  memberId?: string;
-  memberClass?: string;
-  memberGender?: string;
-  memberEng?: string;
-  memberPhone?: string;
-  memberBirth?: string;
+  memberProfile?: string;
+  memberId: string;
+  memberClass: string;
+  memberGender: string;
+  memberEng: string;
+  memberPhone: string;
+  memberBirth: string;
   createdAt?: string;
   updatedAt?: string;
+  universityName: string;
+  records: Record[];
+}
+
+export interface Record {
+  record1st: string;
+  record2nd: string;
+  bibNo: number;
+  ranking: number;
+  totalRecord: string;
+  higherRankRecord?: string;
+  lowerRankRecord?: string;
+  higherRankInfo?: RankInfo;
+  lowerRankInfo?: RankInfo;
+  race: RaceDataType;
+}
+
+export interface RankInfo {
+  memberName: string;
+  bibNo: number;
+  totalRecord: string;
 }
